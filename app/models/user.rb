0623720b2c1e2e2ec832f3df916b157ee27691e9
ivanprytula <ivanprytula87@gmail.com>
2022-06-06 23:34:09 +0300

    add one-to-many association between users & articles from backend

--- app/models/user.rb
+++ app/models/user.rb
@@ -8,4 +8,6 @@ class User < ApplicationRecord
             uniqueness: { case_sensitive: false },
             length: { maximum: 105 },
             format: { with: VALID_EMAIL_REGEX }
+
+  has_many :articles
 end
