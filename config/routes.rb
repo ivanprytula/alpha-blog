e8c3bca14baa2681722aa9a1c8042b89f73384e9
ivanprytula <ivanprytula87@gmail.com>
2022-06-07 00:20:15 +0300

    add new signup form

--- config/routes.rb
+++ config/routes.rb
@@ -4,4 +4,8 @@ Rails.application.routes.draw do
   get 'about', to: 'pages#about'
   resources :articles
   # resources :articles, only: [:show, :index, :new, :create, :edit, :update, :destroy]
+  #
+  get 'signup', to: 'users#new'
+  # post 'users', to: 'users#create'
+  resources :users, except: [ :new ]
 end
