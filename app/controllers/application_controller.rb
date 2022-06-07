bd7471b7e9d69a8671cd15b8c9386ce85b883e5e
ivanprytula <ivanprytula87@gmail.com>
2022-06-08 00:58:34 +0300

    restrict actions fromn UI

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,16 @@
 class ApplicationController < ActionController::Base
+
+  # makes this method available in views as well
+  helper_method :current_user, :logged_in?
+
+  def current_user
+    # memoization technique
+    @current_user ||= User.find(session[:user_id]) if session[:user_id]
+  end
+
+  def logged_in?
+    # return boolean value
+    !!current_user
+  end
+
 end
